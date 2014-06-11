8980b0c0eb5be2f8e8c180815df286434d5b25d8
Dave Rigby <daver@couchbase.com>
2014-06-11 09:49:15 +0100

    WIP: Add high client test
    Change-Id: Ia561b0291be39473fd2e6091424c3dfb9f0e7282

--- perfrunner/tests/multitenancy.py
+++ perfrunner/tests/multitenancy.py
@@ -3,11 +3,31 @@ import time
 import numpy as np
 from logger import logger
 
+from couchbase import Couchbase, FMT_BYTES
+
 from perfrunner.helpers.misc import pretty_dict
 from perfrunner.tests import PerfTest
 
 
-class EmptyBucketsTest(PerfTest):
+class MultiTenancyTest(PerfTest):
+
+    def report_stats(self):
+        cpu = lambda data: round(np.mean(data), 1)
+        rss = lambda data: int(np.mean(data) / 1024 ** 2)
+        conn = lambda data: int(np.mean(data))
+
+        for hostname, s in self.rest.get_node_stats(self.master_node,
+                                                    'bucket-1'):
+            summary = {
+                'memcached, MBytes': rss(s['proc/memcached/mem_resident']),
+                'beam.smp, MBytes': rss(s['proc/(main)beam.smp/mem_resident']),
+                'Total CPU, %': cpu(s['cpu_utilization_rate']),
+                'Curr. connections': conn(s['curr_connections']),
+            }
+            logger.info(pretty_dict({hostname: summary}))
+
+
+class EmptyBucketsTest(MultiTenancyTest):
 
     ITERATION_DELAY = 300
 
@@ -29,21 +49,6 @@ class EmptyBucketsTest(PerfTest):
                                     eviction_policy=eviction_policy,
                                     threads_number=threads_number,
                                     password=password)
-
-    def report_stats(self):
-        cpu = lambda data: round(np.mean(data), 1)
-        rss = lambda data: int(np.mean(data) / 1024 ** 2)
-        conn = lambda data: int(np.mean(data))
-
-        for hostname, s in self.rest.get_node_stats(self.master_node,
-                                                    'bucket-1'):
-            summary = {
-                'memcached, MBytes': rss(s['proc/memcached/mem_resident']),
-                'beam.smp, MBytes': rss(s['proc/(main)beam.smp/mem_resident']),
-                'Total CPU, %': cpu(s['cpu_utilization_rate']),
-                'Curr. connections': conn(s['curr_connections']),
-            }
-            logger.info(pretty_dict({hostname: summary}))
 
     def delete_buckets(self, num_buckets):
         for i in range(num_buckets):
@@ -69,3 +74,66 @@ class EmptyBucketsTest(PerfTest):
             self.monitor.monitor_node_health(self.master_node)
             logger.info('Sleeping {} seconds'.format(self.ITERATION_DELAY / 2))
             time.sleep(self.ITERATION_DELAY / 2)
+
+
+class MultiClientInfrequentTest(MultiTenancyTest):
+    """Tests a large number of clents connecting and accessing the cluster.
+
+    Each client only makes a small number of requests, but there is a
+    large number of clients."""
+
+    def run(self):
+        # Create the bucket
+        bucket_name = self.test_config.buckets[0]
+        ram_quota = self.test_config.cluster.mem_quota
+        replica_number = self.test_config.bucket.replica_number
+        replica_index = self.test_config.bucket.replica_index
+        eviction_policy = self.test_config.bucket.eviction_policy
+        threads_number = self.test_config.bucket.threads_number
+        password = self.test_config.bucket.password
+
+        self.rest.delete_bucket(host_port=self.master_node,
+                                name=bucket_name)
+
+        self.rest.create_bucket(host_port=self.master_node,
+                                name=bucket_name,
+                                ram_quota=ram_quota,
+                                replica_number=replica_number,
+                                replica_index=replica_index,
+                                eviction_policy=eviction_policy,
+                                threads_number=threads_number,
+                                password=password)
+        self.wait_for_persistence()
+        self.monitor.monitor_node_health(self.master_node)
+
+        # Set document from each of the worker connections.
+        conns = list()
+
+        # Create all the connections
+        for i in range(self.test_config.access_settings.workers):
+            logger.info('DJR host ' + self.master_node + ' iter:' + str(i))
+            conns.append(Couchbase.connect(host=self.master_node.split(':')[0],
+                                           port=8091, bucket=bucket_name,
+                                           password=password,
+                                           timeout=30.0))
+
+        # Perform operations on each client. Note this is serial, but given we only
+        # perform a hangful of ops from each client it's probalby ok.
+
+        # 10 MB test document.
+        test_doc = bytearray(10 * 1024 * 1024)
+        for c in conns:
+            # set then get 10MB document.
+            key = 'key-' + str(i)
+            c.set(key, test_doc, format=FMT_BYTES)
+            c.get(key)
+            logger.info("X")
+
+        logger.info("Completed all operations")
+
+        # Monitor
+        self.report_stats()
+
+        # Clean up
+        self.rest.delete_bucket(host_port=self.master_node,
+                                name=bucket_name)
