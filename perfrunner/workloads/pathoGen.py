fcd1c151e97630558c23d559f9b8e2cf46e11d70
Dave Rigby <daver@couchbase.com>
2014-09-08 14:53:06 +0100

    Convert pathoGen to multi-process
    Change-Id: I9de5c59ff2180d7f59a4a153f4aa64e531a23d21

--- perfrunner/workloads/pathoGen.py
+++ perfrunner/workloads/pathoGen.py
@@ -94,14 +94,25 @@ occupied on each page - so 32/8192 bytes are acutally used, with 8160B
 """
 
 import itertools
-import Queue
+import multiprocessing
 import random
 import time
-import threading
 
 from couchbase import Couchbase, FMT_BYTES, exceptions
 from logger import logger
 
+
+# TCMalloc size classes
+SIZES = (8, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192,
+         208, 224, 240, 256, 288, 320, 352, 384, 416, 448, 480,
+         512, 576, 640, 704, 768, 832, 896, 960, 1024, 1152, 1280,
+         1408, 1536, 1792, 2048, 2304, 2560, 2816, 3072, 3328,
+         4096, 4608, 5120, 6144, 6656, 8192, 9216, 10240, 12288,
+         13312, 16384, 20480, 24576, 26624, 32768, 40960, 49152,
+         57344, 65536, 73728, 81920, 90112, 98304, 106496, 114688,
+         122880, 131072, 139264, 147456, 155648, 163840, 172032,
+         180224, 188416, 196608, 204800, 212992, 221184, 229376,
+         237568, 245760, 253952, 262144)
 
 class AlwaysPromote(object):
     """Promotion policy for baseline case - always promote."""
@@ -115,7 +126,7 @@ class Freeze(object):
     def __init__(self, num_items, num_iterations):
         self.num_items = num_items
         # Initialize deterministic pseudo-RNG for when to freeze docs.
-        self.lock = threading.Lock()
+        self.lock = multiprocessing.Lock()
         self.rng = random.Random(0)
         self.freeze_probability = 0.03 / num_iterations
 
@@ -147,10 +158,10 @@ class PathoGen(object):
         # Create queues
         self.queues = list()
         for i in range(self.num_workers):
-            self.queues.append(Queue.Queue())
+            self.queues.append(multiprocessing.Queue())
 
-        # Create and spin up threads.
-        self.threads = list()
+        # Create and spin up workers
+        self.workers = list()
         for i in range(self.num_workers):
             if i == self.num_workers - 1:
                 # Last one is the Supervisor
@@ -166,35 +177,23 @@ class PathoGen(object):
                            in_queue=self.queues[i],
                            out_queue=self.queues[(i + 1) % self.num_workers],
                            promotion_policy=promotion_policy)
-            self.threads.append(t)
+            self.workers.append(t)
 
     def run(self):
         logger.info('Starting PathoGen: {} items, {} workers'.format(
             self.num_items, self.num_workers))
 
-        for t in self.threads:
+        for t in self.workers:
             t.start()
-        for t in self.threads:
+        for t in self.workers:
             t.join()
 
 
-class Worker(threading.Thread):
-
-    # TCMalloc size classes
-    SIZES = (8, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192,
-             208, 224, 240, 256, 288, 320, 352, 384, 416, 448, 480,
-             512, 576, 640, 704, 768, 832, 896, 960, 1024, 1152, 1280,
-             1408, 1536, 1792, 2048, 2304, 2560, 2816, 3072, 3328,
-             4096, 4608, 5120, 6144, 6656, 8192, 9216, 10240, 12288,
-             13312, 16384, 20480, 24576, 26624, 32768, 40960, 49152,
-             57344, 65536, 73728, 81920, 90112, 98304, 106496, 114688,
-             122880, 131072, 139264, 147456, 155648, 163840, 172032,
-             180224, 188416, 196608, 204800, 212992, 221184, 229376,
-             237568, 245760, 253952, 262144)
+class Worker(multiprocessing.Process):
 
     def __init__(self, number, host, port, bucket, in_queue,
                  out_queue, promotion_policy):
-        threading.Thread.__init__(self)
+        super(Worker, self).__init__()
         self.id = number
         self.in_queue = in_queue
         self.out_queue = out_queue
@@ -202,10 +201,10 @@ class Worker(threading.Thread):
         self.client = Couchbase.connect(bucket=bucket, host=host, port=port)
 
         # Pre-generate a buffer of the maximum size to use for constructing documents.
-        self.buffer = bytearray('x' for _ in range(self.SIZES[-1]))
+        self.buffer = bytearray('x' for _ in range(SIZES[-1]))
 
     def run(self):
-        """Run a Worker thread. They run essentially forever, taking document
+        """Run a Worker. They run essentially forever, taking document
         size iterators from the input queue and adding them to the
         output queue for the next guy.
         """
@@ -237,10 +236,21 @@ class Worker(threading.Thread):
                 success = True
             except (exceptions.TimeoutError,
                     exceptions.TemporaryFailError) as e:
-                logger.debug('Thread-{0}: Sleeping for {1}s due to {2}'.format(
+                logger.debug('Worker-{0}: Sleeping for {1}s due to {2}'.format(
                     self.start, backoff, e))
                 time.sleep(backoff)
                 backoff *= 2
+
+
+class SequenceIterator(object):
+    def __init__(self, max_size):
+        self.sizes = list(SIZES[:SIZES.index(max_size)])
+
+    def next(self):
+        if self.sizes:
+            return self.sizes.pop(0)
+        else:
+            raise StopIteration
 
 
 class Supervisor(Worker):
@@ -260,8 +270,7 @@ class Supervisor(Worker):
         """Returns a sequence of sizes which ramps from minimum to maximum
         size.
         """
-        return itertools.islice(self.SIZES[:self.SIZES.index(self.max_size)],
-                                None)
+        return SequenceIterator(self.max_size)
 
     def run(self):
         """Run the Supervisor. This is similar to Worker, except that
